b285c5c225f14eb90d507d1115aac6d7b4eac27e
Luchangas <Luchangas@Lucianos-MacBook-Pro.local>
2015-10-15 22:09:53 -0300

    Funcion de lectura de Menu

---
+++ readMenu.c
@@ -0,0 +1,12 @@
+
+int readMenu(void){
+  int choise;
+
+  printf("\n\tBienvenido al juego 2048\n\n");
+  printf("Menú del Juego:\n");
+  printf("1. Juego Nuevo\n2. Recuperar un juego grabado\n3. Salir\n");
+  printf("Escriba el número de alguna de las opciones del menú: ");
+  scanf("%d",&choise);
+  LIMPIA BUFFER
+  return choise;
+}
